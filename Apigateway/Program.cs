4271fcb65af3b294f8116562cc4ddcaaf170b31d
Gabriel Moré Maia <gabriel_more@hotmail.com>
2022-11-01 16:42:19 -0300

    first commit

---
+++ Apigateway/Program.cs
@@ -0,0 +1,24 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Configuration.AddJsonFile("ocelot.json");
+
+    //custom configs
+    builder.Services.AddOcelot(builder.Configuration);
+}
+
+var app = builder.Build();
+{
+    app.UseHttpsRedirection();
+
+    app.UseOcelot().Wait();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+
